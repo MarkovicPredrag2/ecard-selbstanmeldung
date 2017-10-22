6a84c7874f9f122435b616fbb5f42440f8258eae
Markovic Predrag <markovic.predrag.gs@gmail.com>
2017-10-22 15:30:19 -0700

    zweiter cli commit

--- org/commit.sh
+++ org/commit.sh
@@ -2,10 +2,11 @@
 
 echo "Committext: "
 read committext
-git add ./../.
-git commit -m "$committext"
 echo "Branch: "
 read branch
+git pull $(git remote) master
+git add $(pwd)/..
+git commit -m "$committext"
 git push -u $(git remote) $branch
 
 echo -e "\e[92m\e[1mVorgang abgeschlossen!"
--- org/fetch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mysql -u root -p
-echo "hello world"
