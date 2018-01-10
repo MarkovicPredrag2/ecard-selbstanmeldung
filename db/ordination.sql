1ad5a8af0ac7d3ec44ca5f004e2f306438134256
Seifert <sei16350@spengergasse.at>
2018-01-10 16:52:02 +0100

    Dynamische Warteliste, Werterückgabe bei Patientenauswahl für die Arztansicht

--- db/ordination.sql
+++ db/ordination.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: lokalePatientenDB
 -- ------------------------------------------------------
--- Server version	5.7.20-0ubuntu0.17.10.1
+-- Server version	5.7.20-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -219,4 +219,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-10 13:24:08
+-- Dump completed on 2018-01-10 16:52:01
